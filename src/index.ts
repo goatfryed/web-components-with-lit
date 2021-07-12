2cb50b4586f2e9a033fae84e0f8b7a59a5f30043
Omar Goatfryed Sood <goatfryed@gmail.com>
2021-07-12 15:56:59 +0200

    Use session array to dynamically create sessions
    We look at more js in templates. Also note that you don't have to use the shadowDom

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import "./app/webApp";
+import {html, render} from "lit";
+import {SESSIONS} from "./data";
 
 class WebApp extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `
-            <div>
-                <web-title iteration="34">Kasseler Webmontag</web-title>
-                <hr/>
-                <current-session></current-session>
-                <hr/>
-                <ol>
-                    <li>React Hooks🎣 lifecoding - Nico</li>
-                    <li>Web components mit lit 🔥 - Omar</li>
-                    <li>next level html mit jQuery - Internet Explorer</li>
-                </ol>
-            </div>
-        `
+        render(
+            html`
+                <div>
+                    <web-title iteration="34">Kasseler Webmontag</web-title>
+                    <hr/>
+                    <current-session></current-session>
+                    <hr/>
+                    <ol>
+                        ${ SESSIONS.map( it => html`<li>${it.topic} - by ${it.speaker}</li>`)}
+                    </ol>
+                </div>
+            `,
+            this
+        )
     }
 }
 customElements.define("my-web-app", WebApp)
