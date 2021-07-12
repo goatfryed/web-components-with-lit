96d7d04400d8719eae9aa97e23aa48f0fddabee2
Omar Goatfryed Sood <goatfryed@gmail.com>
2021-07-12 15:42:03 +0200

    extract static html into simple web component
    We create a basic web component by registering a custom element. This custom elements innerHtml is just our prev static html content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 import "./app/webApp";
+
+class WebApp extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+            <div>
+                <h1>35. Kasseler Webmontag</h1>
+                <hr/>
+                <current-session></current-session>
+                <hr/>
+                <ol>
+                    <li>React Hooks🎣 lifecoding - Nico</li>
+                    <li>Web components mit lit 🔥 - Omar</li>
+                    <li>next level html mit jQuery - Internet Explorer</li>
+                </ol>
+            </div>
+        `
+    }
+}
+customElements.define("my-web-app", WebApp)
